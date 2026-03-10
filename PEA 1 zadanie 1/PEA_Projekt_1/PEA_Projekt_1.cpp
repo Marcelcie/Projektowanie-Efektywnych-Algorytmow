1e8dec99beb29af172860f8b3afcef6ace8d98b3
Marcel Cieśliński <cieslinski.marcel@gmail.com>
2026-03-10 19:56:22 +0100

    Interaktywne menu

--- PEA 1 zadanie 1/PEA_Projekt_1/PEA_Projekt_1.cpp
+++ PEA 1 zadanie 1/PEA_Projekt_1/PEA_Projekt_1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void matrix_reader(string file) {
-    
+    //Funkcja, która wczytuje plik .txt z danymi i sprawdza czy podana ścieżka istnieje.
     ifstream plik(file);
         if (plik.is_open()) {
             int N;
@@ -14,7 +14,7 @@ void matrix_reader(string file) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N ; j++) {
                     plik >> matrix[i * N + j];
-                    cout << matrix[i * N + j] << "\t";
+                    //cout << matrix[i * N + j] << "\t";  -> Test poprawności wyświetlania danych z pliku.
                 }
                 cout << endl;
             }
@@ -26,8 +26,55 @@ void matrix_reader(string file) {
         }
 
 }
+void Print_Matrix(int* matrix,int N) {
+    
+    if (matrix != nullptr) {
+        delete[] matrix;
+    }
+
+    int* matrix = new int[N * N];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i * N + j] << "\t";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    matrix_reader("C:\\Users\\ciesl\\source\\repos\\Projektowanie-Efektywnych-Algorytmow\\PEA 1 zadanie 1\\PEA_Projekt_1\\x64\\Release\\tsp_6_2.txt");
+    //deklaracja zmiennych
+    int wybor; string sciezka; int* matrix = nullptr; int N = 0;
+    //koniec deklaracji
+
+    do {
+        //Interaktywne menu
+        cout << "\n=== MENU GLOWNE ===" << endl;
+        cout << "1. Wczytaj macierz z pliku" << endl;
+        cout << "2. Wyswietl macierz" << endl;
+        // ... dopisz reszte opcji (NN, Brute-Force, itp.)
+        cout << "0. Wyjscie" << endl;
+        cout << "Wybierz opcje: ";
+        cin >> wybor;
+        switch (wybor) {
+        case 1:
+            cout << "Podaj sciezke do swojego pliku .txt: \n";
+            cin.ignore();          // Czyścimy bufor z Entera po 'cin >> wybor'
+            getline(cin, sciezka); // Pobieramy pełną ścieżkę ze spacjami
+            cout << "Wczytano sciezke do pamieci programu." << endl;
+            break;
+        case 2:
+            cout << endl;
+            //C:\\Users\\ciesl\\source\\repos\\Projektowanie-Efektywnych-Algorytmow\\PEA 1 zadanie 1\\PEA_Projekt_1\\x64\\Release\\tsp_6_2.txt -> przykładowy plik do wczytywania.
+            Print_Matrix(matrix,N);
+            break;
+        default:
+            cout << "Blad nieprawidlowa liczba!";
+        }
+    } while(wybor != 0);
+
+    //zwalnianie pamięci przed zakończeniem programu.
+    if (matrix != nullptr) {
+        delete[] matrix;
+    }
 }
